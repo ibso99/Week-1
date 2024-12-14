d68bd2d5b4ba9c2499479abd92e0d2cbdb303427
ibso99 <123656921+ibso99@users.noreply.github.com>
2024-12-14 20:48:14 +0300

    performed basic EDA and Time Series Analysis

--- scripts/Data_Loader.py
+++ scripts/Data_Loader.py
@@ -1,17 +1,6 @@
 import pandas as pd 
 
-AAPL_PATH = "C:\Users\ibsan\Desktop\TenX\week-1\Data\yfinance_data\AAPL_historical_data.csv"
-AMZN_PATH = "C:\Users\ibsan\Desktop\TenX\week-1\Data\yfinance_data\AMZN_historical_data.csv"
-GOOG_PATH = "C:\Users\ibsan\Desktop\TenX\week-1\Data\yfinance_data\GOOG_historical_data.csv"
-META_PATH = "C:\Users\ibsan\Desktop\TenX\week-1\Data\yfinance_data\META_historical_data.csv"
-MSFT_PATH = "C:\Users\ibsan\Desktop\TenX\week-1\Data\yfinance_data\MSFT_historical_data.csv"
-NVDA_PATH = "C:\Users\ibsan\Desktop\TenX\week-1\Data\yfinance_data\NVDA_historical_data.csv"
-TSLA_PATH = "C:\Users\ibsan\Desktop\TenX\week-1\Data\yfinance_data\TSLA_historical_data.csv"
-
-AAPL_DF = pd.read_csv(AAPL_PATH)
-AMZN_DF = pd.read_csv(AMZN_PATH)
-GOOG_DF = pd.read_csv(GOOG_PATH)
-META_DF = pd.read_csv(META_PATH)
-MSFT_DF = pd.read_csv(MSFT_PATH)
-NVDA_DF = pd.read_csv(NVDA_PATH)
-TSLA_DF = pd.read_csv(TSLA_PATH)
+def Data_Loader(file_path):
+    ''' creating a 
+    dataloader function '''
+    return pd.read_csv(file_path, parse_dates=['Date'])
